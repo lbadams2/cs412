0c860a73f9f6d6a86b5b20acb22514d11a73a5db
Liam Adams <lbadams2@ncsu.edu>
2018-12-01 23:32:19 -0600

    update decision tree

--- ladam5_assign4/python3_code/DecisionTree.py
+++ ladam5_assign4/python3_code/DecisionTree.py
@@ -13,6 +13,14 @@ class Node:
         self.left_split_vals = None
         self.right_split_vals = None
 
+class Full_Node:
+    def __init__(self, data):
+        self.children = []
+        self.data = data
+        self.split_attr = None
+        self.val = None
+        self.child_vals = set()
+
 class Split_Data:
     def __init__(self, data, attr, attr_vals_unique, class_probs, val_subset, is_positive):
         self.data = data
@@ -24,7 +32,7 @@ class Split_Data:
 
 def gini_index(class_probs):
     summation = 0
-    for prob in class_probs.values():
+    for prob in class_probs:
         summation = summation + prob*prob
     return 1 - summation
 
@@ -82,6 +90,109 @@ def create_split_data(table, subset, second_subset, split_attr):
     second_split_data = Split_Data(second_split_table, split_attr, second_split_attr_unique_vals, second_class_probs, second_subset, False)
     return first_split_data, second_split_data
 
+def gini_index_attr_full(attr_val_table, attr_vals_unique):
+    # maybe optimize to not call len()
+    total_count = len(attr_val_table.keys())
+    min_attr_gini_index = None
+    no_unique_vals = False
+
+    split_tables = {}
+    class_counts = {}
+    split_counts = {}
+    split_class_probs = {}
+    split_attr_vals_unique = {}
+
+    for tuple_num, v in attr_val_table.items():
+        for attr, val in v.items():
+            # don't split on attributes already split on or with only one value
+            if attr not in attr_vals_unique or len(attr_vals_unique[attr]) == 1:
+                continue
+            if attr not in split_tables:
+                split_tables[attr] = {}
+                split_tables[attr][val] = {}
+                split_tables[attr][val][tuple_num] = {}
+                split_tables[attr][val][tuple_num] = v
+                class_counts[attr] = {}
+                class_counts[attr][val] = {}
+                class_counts[attr][val][v['class']] = 1
+                split_counts[attr] = {}
+                split_counts[attr][val] = 1
+                split_class_probs[attr] = {}
+                split_class_probs[attr][val] = {}
+                split_class_probs[attr][val][v['class']] = class_counts[attr][val][v['class']] / split_counts[attr][val]
+                split_attr_vals_unique[attr] = {}
+                split_attr_vals_unique[attr][val] = {}
+            elif val not in split_tables[attr]:
+                split_tables[attr][val] = {}
+                split_tables[attr][val][tuple_num] = {}
+                split_tables[attr][val][tuple_num] = v
+                class_counts[attr][val] = {}
+                class_counts[attr][val][v['class']] = 1
+                split_counts[attr][val] = 1
+                split_class_probs[attr][val] = {}
+                split_class_probs[attr][val][v['class']] = class_counts[attr][val][v['class']] / split_counts[attr][val]
+                split_attr_vals_unique[attr][val] = {}
+            elif tuple_num not in split_tables[attr][val]:
+                split_tables[attr][val][tuple_num] = {}
+                split_tables[attr][val][tuple_num] = v
+                if v['class'] not in class_counts[attr][val]:
+                    class_counts[attr][val][v['class']] = 1
+                else:
+                    class_counts[attr][val][v['class']] = class_counts[attr][val][v['class']] + 1
+                split_counts[attr][val] = split_counts[attr][val] + 1
+                split_class_probs[attr][val][v['class']] = class_counts[attr][val][v['class']] / split_counts[attr][val]
+        
+            for other_attr, other_val in v.items():
+                if other_attr == 'class':
+                    continue
+                if other_attr != attr:
+                    if other_attr not in split_attr_vals_unique[attr][val]:
+                        split_attr_vals_unique[attr][val][other_attr] = set()
+                        split_attr_vals_unique[attr][val][other_attr].add(other_val)
+                    else:
+                        if other_val not in split_attr_vals_unique[attr][val][other_attr]:
+                            split_attr_vals_unique[attr][val][other_attr].add(other_val)    
+
+    # delete unique vals of length 1
+    for outer_vals in split_attr_vals_unique.values():
+        for unique_val_dicts in outer_vals.values():
+            temp_dict = unique_val_dicts.copy()
+            for unique_attr, unique_vals in temp_dict.items():                 
+                if len(unique_vals) < 2:
+                    del unique_val_dicts[unique_attr]
+
+    attr_gini_indexes = {}
+    min_attr = None
+    min_attr_gini_index = -1
+    for attr, vals in attr_vals_unique.items():
+        for val in vals:
+            probs = []
+            for prob in split_class_probs[attr][val].values():
+                probs.append(prob)
+            gini_index_val = gini_index(probs)
+            split_ratio = split_counts[attr][val] / total_count
+            split_gini_index = split_ratio * gini_index_val
+            if attr not in attr_gini_indexes:
+                attr_gini_indexes[attr] = split_gini_index
+            else:
+                attr_gini_indexes[attr] = attr_gini_indexes[attr] + split_gini_index
+            
+            if not min_attr:
+                min_attr = attr
+                min_attr_gini_index = attr_gini_indexes[attr]
+            else:
+                if attr_gini_indexes[attr] < min_attr_gini_index:
+                    min_attr = attr
+                    min_attr_gini_index = attr_gini_indexes[attr]
+    
+    splits = split_tables[min_attr]
+    split_data_list = []
+    for val, table in splits.items():
+        split_data = Split_Data(table, min_attr, split_attr_vals_unique[min_attr][val], split_class_probs[min_attr][val], set(val), False)
+        split_data_list.append(split_data)
+
+    return split_data_list
+
 def gini_index_attr(attr_val_table, attr_vals_unique):
     # maybe optimize to not call len()
     total_count = len(attr_val_table.keys())
@@ -113,7 +224,7 @@ def gini_index_attr(attr_val_table, attr_vals_unique):
 
             first_split_ratio = len(first_split_data.data.keys())/total_count
             second_split_ratio = len(second_split_data.data.keys())/total_count
-            subset_gini_index = first_split_ratio * gini_index(first_split_data.class_probs) + second_split_ratio * gini_index(second_split_data.class_probs)
+            subset_gini_index = first_split_ratio * gini_index(first_split_data.class_probs.values()) + second_split_ratio * gini_index(second_split_data.class_probs.values())
 
             if not subset_gini_index_min or subset_gini_index < subset_gini_index_min:
                 subset_gini_index_min = subset_gini_index
@@ -125,7 +236,37 @@ def gini_index_attr(attr_val_table, attr_vals_unique):
     
     return best_split, no_unique_vals
 
-def generate_decision_tree(table, attr_list, class_probs):
+def generate_full_decision_tree(table, attr_list, class_probs, val):
+    node = Full_Node(table)
+    node.val = val
+    if len(class_probs.keys()) == 1:
+        node.class_label = list(class_probs.keys())[0]
+        return node
+
+    # how to break tie
+    majority_class = max(class_probs, key=class_probs.get)
+    if not attr_list:        
+        node.class_label = majority_class
+        return node
+
+    split_data_list = gini_index_attr_full(table, attr_list)
+    split_attr = split_data_list[0].attr
+    node.split_attr = split_attr
+    for split_data in split_data_list:
+        node.child_vals.update(split_data.subset)
+        if not split_data.data:
+            leaf_node = Full_Node(None)
+            leaf_node.class_label = majority_class
+            leaf_node.split_attr = split_data.attr
+            leaf_node.val = split_data.subset
+            node.children.append(leaf_node)
+        else:
+            split_val = split_data.subset.pop()
+            split_data.subset.add(split_val)
+            node.children.append(generate_full_decision_tree(split_data.data, split_data.attr_vals_unique, split_data.class_probs, split_val))
+
+
+def generate_binary_decision_tree(table, attr_list, class_probs):
     node = Node(table)
     if len(class_probs.keys()) == 1:
         node.class_label = list(class_probs.keys())[0]
@@ -156,14 +297,14 @@ def generate_decision_tree(table, attr_list, class_probs):
         leaf_node.class_label = majority_class
         node.l = leaf_node
     else:
-        node.l = generate_decision_tree(yes_split.data, yes_split.attr_vals_unique, yes_split.class_probs)
+        node.l = generate_binary_decision_tree(yes_split.data, yes_split.attr_vals_unique, yes_split.class_probs)
 
     if not no_split.data:
         leaf_node = Node(None)
         leaf_node.class_label = majority_class
         node.r = leaf_node
     else:
-        node.r = generate_decision_tree(no_split.data, no_split.attr_vals_unique, no_split.class_probs)
+        node.r = generate_binary_decision_tree(no_split.data, no_split.attr_vals_unique, no_split.class_probs)
 
     return node
 
@@ -210,7 +351,48 @@ def process_training_file(training_file):
     return attr_vals_table, attr_vals_unique, total_class_probs
 
 
-def classify_test_file(decision_tree, test_file):
+def classify_test_file_full(decision_tree, test_file):
+    confusion_matrix = {}
+    with open(test_file, 'r') as f:
+        for line in f:
+            temp_tree = decision_tree
+            line = line.strip()       
+            elems = line.split(' ')
+            class_label = elems[0]
+            tuple_dict = {}
+            for elem in elems[1:]:
+                kv = elem.split(':')
+                tuple_dict[kv[0]] = kv[1]
+            while not temp_tree.class_label:
+                tuple_val = tuple_dict[temp_tree.split_attr]
+                if tuple_val in temp_tree.split_vals:
+                    for node in temp_tree.children:
+                        if tuple_val == node.val:
+                            temp_tree = node
+                            break
+                else:
+                    num = randint(0, len(temp_tree.children) - 1)
+                    temp_tree = temp_tree.children[num]
+            
+            predicted_class = temp_tree.class_label
+            if class_label not in confusion_matrix:
+                confusion_matrix[class_label] = {}
+            if predicted_class not in confusion_matrix[class_label]:
+                confusion_matrix[class_label][predicted_class] = 1
+            else:
+                confusion_matrix[class_label][predicted_class] = confusion_matrix[class_label][predicted_class] + 1
+
+    actual_labels = confusion_matrix.keys()
+    for actual_label in actual_labels:
+        predicted_labels = confusion_matrix[actual_label].keys()
+        diff = actual_labels ^ predicted_labels
+        for missing_label in diff:
+            confusion_matrix[actual_label][missing_label] = 0
+        
+    return confusion_matrix
+    
+
+def classify_test_file_binary(decision_tree, test_file):
     confusion_matrix = {}
     with open(test_file, 'r') as f:
         for line in f:
@@ -270,15 +452,21 @@ def print_output(confusion_matrix):
 
 #training_file = sys.argv[1]
 #test_file = sys.argv[2]
-training_file = '../ladam5_assign4/data/led.train'
-test_file = '../ladam5_assign4/data/led.test'
+training_file = '../ladam5_assign4/data/balance.scale.train'
+test_file = '../ladam5_assign4/data/balance.scale.test'
 processed_data = process_training_file(training_file)
 processed_table = processed_data[0]
 processed_unique_vals = processed_data[1]
 processed_class_probs = processed_data[2]
+matrix = None
 start = timer()
-tree = generate_decision_tree(processed_table, processed_unique_vals, processed_class_probs)
+if 'balance' in training_file or 'synthetic' in training_file:
+    tree = generate_full_decision_tree(processed_table, processed_unique_vals, processed_class_probs, None)
+    matrix = classify_test_file_full(tree, test_file)
+elif 'led' in training_file or 'nursery' in training_file:
+    tree = generate_binary_decision_tree(processed_table, processed_unique_vals, processed_class_probs)
+    matrix = classify_test_file_binary(tree, test_file)
+tree = generate_binary_decision_tree(processed_table, processed_unique_vals, processed_class_probs)
 end = timer()
 #print(end-start)
-matrix = classify_test_file(tree, test_file)
 print_output(matrix)
